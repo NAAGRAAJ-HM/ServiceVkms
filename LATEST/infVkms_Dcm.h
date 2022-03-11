b4dffc00fb45bee940046c9ca0451091bd07c39d
root <nagaraja.hm.0001@gmail.com>
2022-03-11 12:13:53 +0100

    Update: AUTOSAR basic software service module Vkms

--- LATEST/infVkms_Dcm.h
+++ LATEST/infVkms_Dcm.h
@@ -1,15 +1,13 @@
+#pragma once
 /*****************************************************/
-/* File   : Vkms.cpp                                 */
+/* File   : infVkms_Dcm.h                            */
 /* Author : Naagraaj HM                              */
 /*****************************************************/
 
 /*****************************************************/
 /* #INCLUDES                                         */
 /*****************************************************/
-#include "module.h"
-#include "infVkms_EcuM.h"
-#include "infVkms_Dcm.h"
-#include "infVkms_SchM.h"
+#include "Compiler_Cfg_Vkms.h"
 
 /*****************************************************/
 /* #DEFINES                                          */
@@ -22,15 +20,6 @@
 /*****************************************************/
 /* TYPEDEFS                                          */
 /*****************************************************/
-class module_Vkms:
-      public abstract_module
-{
-   public:
-      FUNC(void, VKMS_CODE) InitFunction   (void);
-      FUNC(void, VKMS_CODE) DeInitFunction (void);
-      FUNC(void, VKMS_CODE) GetVersionInfo (void);
-      FUNC(void, VKMS_CODE) MainFunction   (void);
-};
 
 /*****************************************************/
 /* CONSTS                                            */
@@ -43,27 +32,11 @@ class module_Vkms:
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-module_Vkms    Vkms;
-infEcuMClient* gptrinfEcuMClient_Vkms = &Vkms;
-infDcmClient*  gptrinfDcmClient_Vkms  = &Vkms;
-infSchMClient* gptrinfSchMClient_Vkms = &Vkms;
+extern infDcmClient* gptrinfDcmClient_Vkms;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-FUNC(void, VKMS_CODE) module_Vkms::InitFunction(void){
-}
-
-FUNC(void, VKMS_CODE) module_Vkms::DeInitFunction(void){
-}
-
-FUNC(void, VKMS_CODE) module_Vkms::GetVersionInfo(void){
-}
-
-FUNC(void, VKMS_CODE) module_Vkms::MainFunction(void){
-}
-
-#include "Vkms_Unused.h"
 
 /*****************************************************/
 /* EOF                                               */
